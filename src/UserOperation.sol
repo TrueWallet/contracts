2aef63b662a4b131a44007a00c9d0c26bd19e1a0
VitalR <vitalii.rodikov@gmail.com>
2023-03-20 21:26:45 +0200

    add AA EntryPoint contracts

--- src/UserOperation.sol
+++ src/UserOperation.sol
@@ -9,11 +9,12 @@ pragma solidity ^0.8.17;
      * @param nonce unique value the sender uses to verify it is not a replay.
      * @param initCode if set, the account contract will be created by this constructor
      * @param callData the method call to execute on this account.
+     * @param callGasLimit the gas limit passed to the callData method call.
      * @param verificationGasLimit gas used for validateUserOp and validatePaymasterUserOp
      * @param preVerificationGas gas not calculated by the handleOps method, but added to the gas paid. Covers batch overhead.
      * @param maxFeePerGas same as EIP-1559 gas parameter
      * @param maxPriorityFeePerGas same as EIP-1559 gas parameter
-     * @param paymasterAndData if set, this field hold the paymaster address and "paymaster-specific-data". the paymaster will pay for the transaction instead of the sender
+     * @param paymasterAndData if set, this field hold the paymaster address and "paymaster-specific-data". the paymaster will pay for the transaction instead of the sender.
      * @param signature sender-verified signature over the entire request, the EntryPoint address and the chain ID.
      */
     struct UserOperation {
@@ -42,15 +43,15 @@ library UserOperationLib {
     // relayer/miner might submit the TX with higher priorityFee, but the user should not
     // pay above what he signed for.
     function gasPrice(UserOperation calldata userOp) internal view returns (uint256) {
-    unchecked {
-        uint256 maxFeePerGas = userOp.maxFeePerGas;
-        uint256 maxPriorityFeePerGas = userOp.maxPriorityFeePerGas;
-        if (maxFeePerGas == maxPriorityFeePerGas) {
-            //legacy mode (for networks that don't support basefee opcode)
-            return maxFeePerGas;
+        unchecked {
+            uint256 maxFeePerGas = userOp.maxFeePerGas;
+            uint256 maxPriorityFeePerGas = userOp.maxPriorityFeePerGas;
+            if (maxFeePerGas == maxPriorityFeePerGas) {
+                // legacy mode (for networks that don't support basefee opcode)
+                return maxFeePerGas;
+            }
+            return min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
         }
-        return min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
-    }
     }
 
     function pack(UserOperation calldata userOp) internal pure returns (bytes memory ret) {
