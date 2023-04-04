b68e060895838a3a02121a7a49c2adf641d3eefe
VitalR <vitalii.rodikov@gmail.com>
2023-04-04 20:01:39 +0300

    test: add e2e tests for erc721 & erc1155

--- config/MumbaiConfig.sol
+++ config/MumbaiConfig.sol
@@ -7,7 +7,7 @@ library MumbaiConfig {
     address public constant ENTRY_POINT = 0xD0b4bD942f6874adb2Fe59870ba7F9441afa93d6;
     address public constant PAYMASTER = 0x1f61AD9Ce28F6A97bdcfe185918a0B7B116D4E3B;
     address public constant FACTORY = 0xa5586a932a2F3E0148fAa5d1B772b9BF02EEE9EE;
-    address public constant WALLET = 0x3722C008Eb0ce8ba68542C9f3D55ebC132E31210;
+    address public constant WALLET = 0xf041f38D1961C99A8925e88Fc978EdfD31c5264b; // 0x3722C008Eb0ce8ba68542C9f3D55ebC132E31210
 
     address public constant DEPLOYER = 0x8B2dc96fBEd0452f1386C8c2bfE713f11F88D623;
     address public constant WALLET_OWNER = 0x8B2dc96fBEd0452f1386C8c2bfE713f11F88D623;
---
+++ test/fork/ERC1155TransferNoPaymaster.t.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+import {IWallet} from "src/wallet/IWallet.sol";
+import {IWalletFactory} from "src/wallet/IWalletFactory.sol";
+import {IEntryPoint} from "src/interfaces/IEntryPoint.sol";
+import {ITruePaymaster} from "src/paymaster/ITruePaymaster.sol";
+import {UserOperation} from "src/interfaces/UserOperation.sol";
+import {createSignature} from "test/utils/createSignature.sol";
+import {getUserOpHash} from "test/utils/getUserOpHash.sol";
+import {MumbaiConfig} from "config/MumbaiConfig.sol";
+import {MockERC1155} from "../mock/MockERC1155.sol";
+
+contract ERC1155TransferNoPaymasterEntToEndTest is Test {
+    IEntryPoint public constant entryPoint = IEntryPoint(MumbaiConfig.ENTRY_POINT);
+    IWallet public constant wallet = IWallet(MumbaiConfig.WALLET);
+    ITruePaymaster public constant paymaster = ITruePaymaster(MumbaiConfig.PAYMASTER);
+
+    address payable public beneficiary = payable(MumbaiConfig.BENEFICIARY);
+    uint256 ownerPrivateKey = vm.envUint("PRIVATE_KEY_TESTNET");
+    address walletOwner = MumbaiConfig.WALLET_OWNER;
+
+    MockERC1155 public token;
+
+    // Test case
+    bytes32 public userOpHash;
+    address aggregator;
+    uint256 missingWalletFunds;
+    address recipient = 0x9fD12be3448d73c4eF4B0ae189E090c4FD83C9A1;
+    uint256 tokenId;
+    uint256 amount;
+
+    UserOperation public userOp;
+
+    function setUp() public {
+        // 0. Deploy a MockERC721 and fund smart wallet with tokens
+        tokenId = 1;
+        amount = 100;
+        token = new MockERC1155();
+        token.mint(address(wallet), tokenId, amount, "");
+
+        // 1. Generate a userOperation
+        // UserOperation callData transfers a tokenTransferAmount of token to recipient
+        userOp = UserOperation({
+            sender: address(wallet),
+            nonce: wallet.nonce(),
+            initCode: "",
+            callData: "",
+            callGasLimit: 70_000,
+            verificationGasLimit: 958666,
+            preVerificationGas: 115256,
+            maxFeePerGas: 1000105660,
+            maxPriorityFeePerGas: 1000000000,
+            paymasterAndData: "",
+            signature: ""
+        });
+
+        // 2. Encode userOperation transfer
+        userOp.callData = abi.encodeWithSelector(
+            wallet.execute.selector,
+            address(token),
+            0,
+            abi.encodeWithSelector(
+                token.safeTransferFrom.selector,
+                address(wallet),
+                recipient,
+                tokenId,
+                amount,
+                ""
+            )
+        );
+
+        // Sign userOperation and attach signature
+        userOpHash = entryPoint.getUserOpHash(userOp);
+        bytes memory signature = createSignature(userOp, userOpHash, ownerPrivateKey, vm);
+        userOp.signature = signature;
+
+        // Set remainder of test case
+        aggregator = address(0);
+        missingWalletFunds = 1096029019333521;
+
+        // 3. Fund deployer with ETH
+        vm.deal(address(wallet), 5 ether);
+    }
+
+    /// @notice Validate that the smart wallet can validate a userOperation
+    function testWalletValidateUserOp() public {
+        vm.prank(address(entryPoint));
+        wallet.validateUserOp(userOp, userOpHash, aggregator, missingWalletFunds);
+    }
+
+    /// @notice Validate that the EntryPoint can execute a userOperation.
+    ///         No Paymaster, smart wallet pays for gas
+    function testHandleOpsNoPaymaster() public {
+        assertEq(token.balanceOf(recipient, tokenId), 0);
+        assertEq(token.balanceOf(address(wallet), tokenId), amount);
+        uint256 initialWalletETHBalance = address(wallet).balance;
+
+        UserOperation[] memory userOps = new UserOperation[](1);
+        userOps[0] = userOp;
+
+        // Transfer tokens through the entryPoint
+        entryPoint.handleOps(userOps, beneficiary);
+
+        // Verify token transfer from wallet to recipient
+        assertEq(token.balanceOf(address(wallet), tokenId), 0);
+        assertEq(token.balanceOf(recipient, tokenId), amount);
+
+        // Verify wallet paid for gas
+        uint256 walletEthLoss = initialWalletETHBalance - address(wallet).balance;
+        assertGt(walletEthLoss, 0);
+    }
+}
