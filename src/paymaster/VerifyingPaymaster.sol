0694c96267e10cfe083bc04c678b0ea4b7cd1b81
VitalR <vitalii.rodikov@gmail.com>
2023-04-15 14:44:42 +0300

    feat: add VerifyingPaymaster

---
+++ src/paymaster/VerifyingPaymaster.sol
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+/* solhint-disable reason-string */
+/* solhint-disable no-inline-assembly */
+
+import {ITruePaymaster} from "./ITruePaymaster.sol";
+import {IEntryPoint} from "../interfaces/IEntryPoint.sol";
+import {UserOperationLib, UserOperation} from "../interfaces/UserOperation.sol";
+import {ECDSA} from "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+import "src/helper/Helpers.sol";
+
+import "lib/forge-std/src/console.sol";
+
+/**
+ * A paymaster that uses external service to decide whether to pay for the UserOp.
+ * The paymaster trusts an external signer to sign the transaction.
+ * The calling user must pass the UserOp to that external signer first, which performs
+ * whatever off-chain verification before signing the UserOp.
+ * Note that this signature is NOT a replacement for the account-specific signature:
+ * - the paymaster checks a signature to agree to PAY for GAS.
+ * - the account checks a signature to prove identity and account ownership.
+ */
+contract VerifyingPaymaster is ITruePaymaster, Owned {
+    using ECDSA for bytes32;
+    using UserOperationLib for UserOperation;
+
+    IEntryPoint public entryPoint;
+
+    address public immutable verifyingSigner;
+
+    uint256 private constant VALID_TIMESTAMP_OFFSET = 20;
+
+    uint256 private constant SIGNATURE_OFFSET = 84;
+
+    mapping(address => uint256) public senderNonce;
+
+    event UpdateEntryPoint(address indexed _newEntryPoint, address indexed _oldEntryPoint);
+
+    /// @notice Validate that only the entryPoint is able to call a method
+    modifier onlyEntryPoint() {
+        if (msg.sender != address(entryPoint)) {
+            revert InvalidEntryPoint();
+        }
+        _;
+    }
+
+    /// @dev Reverts in case not valid entryPoint or owner
+    error InvalidEntryPoint();
+
+    constructor(IEntryPoint _entryPoint, address _verifyingSigner, address _owner) Owned(_owner) {
+        entryPoint = IEntryPoint(_entryPoint);
+        verifyingSigner = _verifyingSigner;
+    }
+
+    /// @notice Get the total paymaster stake on the entryPoint
+    function getStake() public view returns (uint112) {
+        return entryPoint.getDepositInfo(address(this)).stake;
+    }
+
+    /// @notice Get the total paymaster deposit on the entryPoint
+    function getDeposit() public view returns (uint112) {
+        return entryPoint.getDepositInfo(address(this)).deposit;
+    }
+
+    /// @notice Set the entrypoint contract, restricted to onlyOwner
+    function setEntryPoint(address _newEntryPoint) external onlyOwner {
+        emit UpdateEntryPoint(_newEntryPoint, address(entryPoint));
+        entryPoint = IEntryPoint(_newEntryPoint);
+    }
+
+    ////// VALIDATE USER OPERATIONS
+
+    /**
+     * Verify our external signer signed this request.
+     * the "paymasterAndData" is expected to be the paymaster and a signature over the entire request params
+     * paymasterAndData[:20] : address(this)
+     * paymasterAndData[20:84] : abi.encode(validUntil, validAfter)
+     * paymasterAndData[84:] : signature
+     */
+    function validatePaymasterUserOp(
+        UserOperation calldata userOp,
+        bytes32 /*userOpHash*/,
+        uint256 requiredPreFund
+    ) external returns (bytes memory context, uint256 validationData) {
+        // (requiredPreFund);
+        (uint48 validUntil, uint48 validAfter, bytes calldata signature) = parsePaymasterAndData(userOp.paymasterAndData);
+        // ECDSA library supports both 64 and 65-byte long signatures.
+        // we only "require" it here so that the revert reason on invalid signature will be of "VerifyingPaymaster", and not "ECDSA"
+        require(signature.length == 64 || signature.length == 65, "VerifyingPaymaster: invalid signature length in paymasterAndData");
+        bytes32 hash = ECDSA.toEthSignedMessageHash(getHash(userOp, validUntil, validAfter));
+        senderNonce[userOp.getSender()]++;
+
+        // don't revert on signature failure: return SIG_VALIDATION_FAILED
+        if (verifyingSigner != ECDSA.recover(hash, signature)) {
+            console.log("SIG_VALIDATION_FAILED");
+            return ("", _packValidationData(true,validUntil,validAfter));
+        }
+
+        // no need for other on-chain validation: entire UserOp should have been checked
+        // by the external service prior to signing it.
+        return ("", _packValidationData(false,validUntil,validAfter));
+    }
+
+    /**
+     * Return the hash we're going to sign off-chain (and validate on-chain)
+     * this method is called by the off-chain service, to sign the request.
+     * it is called on-chain from the validatePaymasterUserOp, to validate the signature.
+     * note that this signature covers all fields of the UserOperation, except the "paymasterAndData",
+     * which will carry the signature itself.
+     */
+    function getHash(UserOperation calldata userOp, uint48 validUntil, uint48 validAfter)
+    public view returns (bytes32) {
+        //can't use userOp.hash(), since it contains also the paymasterAndData itself.
+
+        return keccak256(abi.encode(
+                pack(userOp),
+                block.chainid,
+                address(this),
+                senderNonce[userOp.getSender()],
+                validUntil,
+                validAfter
+            ));
+    }
+
+    function parsePaymasterAndData(
+        bytes calldata paymasterAndData
+    ) public pure returns(uint48 validUntil, uint48 validAfter, bytes calldata signature) {
+        (validUntil, validAfter) = abi.decode(paymasterAndData[VALID_TIMESTAMP_OFFSET:SIGNATURE_OFFSET],(uint48, uint48));
+        signature = paymasterAndData[SIGNATURE_OFFSET:];
+    }
+
+    function pack(UserOperation calldata userOp) internal pure returns (bytes memory ret) {
+        // lighter signature scheme
+        bytes calldata pnd = userOp.paymasterAndData;
+        // copy directly the userOp from calldata up to (but not including) the paymasterAndData.
+        // this encoding depends on the ABI encoding of calldata, but is much lighter to copy
+        // than referencing each field separately.
+        assembly {
+            let ofs := userOp
+            let len := sub(sub(pnd.offset, ofs), 32)
+            ret := mload(0x40)
+            mstore(0x40, add(ret, add(len, 32)))
+            mstore(ret, len)
+            calldatacopy(add(ret, 32), ofs, len)
+        }
+    }
+
+    /// @notice Handler for charging the sender (smart wallet) for the transaction after it has been paid for by the paymaster
+    function postOp(
+        PostOpMode mode,
+        bytes calldata context,
+        uint256 actualGasCost
+    ) external onlyEntryPoint {}
+
+    ///// STAKE MANAGEMENT
+
+    /// @notice Add stake for this paymaster to the EntryPoint. Used to allow the paymaster to operate and prevent DDOS
+    function addStake(uint32 _unstakeDelaySeconds) external payable onlyOwner {
+        entryPoint.addStake{value: msg.value}(_unstakeDelaySeconds);
+    }
+
+    /// @notice Unlock paymaster stake
+    function unlockStake() external onlyOwner {
+        entryPoint.unlockStake();
+    }
+
+    /// @notice Withdraw paymaster stake, after having unlocked
+    function withdrawStake(address payable to) external onlyOwner {
+        entryPoint.withdrawStake(to);
+    }
+
+    ///// DEPOSIT MANAGEMENT
+
+    /// @notice Add a deposit for this paymaster to the EntryPoint. Deposit is used to pay user gas fees
+    function deposit() external payable {
+        entryPoint.depositTo{value: msg.value}(address(this));
+    }
+
+    /// @notice Withdraw paymaster deposit to an address
+    function withdraw(address payable to, uint256 amount) external onlyOwner {
+        entryPoint.withdrawTo(to, amount);
+    }
+
+    /// @notice Withdraw all paymaster deposit to an address
+    function withdrawAll(address payable to) external onlyOwner {
+        uint112 totalDeposit = getDeposit();
+        entryPoint.withdrawTo(to, totalDeposit);
+    }
+}
---
+++ test/unit/VerifyingPaymaster.t.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+import {Paymaster} from "src/paymaster/Paymaster.sol";
+import {TrueWallet} from "src/wallet/TrueWallet.sol";
+import {UserOperation} from "src/interfaces/UserOperation.sol";
+import {EntryPoint} from "src/entrypoint/EntryPoint.sol";
+import {IPaymaster} from "src/interfaces/IPaymaster.sol";
+import {IEntryPoint} from "src/interfaces/IEntryPoint.sol";
+import {VerifyingPaymaster} from "src/paymaster/VerifyingPaymaster.sol";
+import {createSignature, createSignature2} from "test/utils/createSignature.sol";
+import "src/helper/Helpers.sol";
+
+import "lib/forge-std/src/console.sol";
+
+contract VerifyingPaymasterUnitTest is Test {
+    TrueWallet wallet;
+    VerifyingPaymaster paymaster;
+    EntryPoint entryPoint;
+    address ownerAddress = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720; // envil account (9)
+    uint256 ownerPrivateKey =
+        uint256(
+            0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
+        );
+    EntryPoint newEntryPoint;
+    address user = address(12);
+    address notOwner = address(13);
+
+    function setUp() public {
+        entryPoint = new EntryPoint();
+        wallet = new TrueWallet(address(entryPoint), ownerAddress);
+        paymaster = new VerifyingPaymaster(entryPoint, ownerAddress, ownerAddress);
+    }
+
+    function testSetupState() public {
+        assertEq(paymaster.owner(), address(ownerAddress));
+        assertEq(paymaster.verifyingSigner(), address(ownerAddress));
+        assertEq(address(paymaster.entryPoint()), address(entryPoint));
+    }
+
+    function testUpdateEntryPoint() public {
+        assertEq(address(paymaster.entryPoint()), address(entryPoint));
+        newEntryPoint = new EntryPoint();
+        vm.prank(address(ownerAddress));
+        paymaster.setEntryPoint(address(newEntryPoint));
+        assertEq(address(paymaster.entryPoint()), address(newEntryPoint));
+    }
+
+    function testUpdateEntryPointNotOwner() public {
+        newEntryPoint = new EntryPoint();
+        vm.prank(address(notOwner));
+        vm.expectRevert();
+        paymaster.setEntryPoint(address(newEntryPoint));
+        assertEq(address(paymaster.entryPoint()), address(entryPoint));
+    }
+
+    function testDeposit() public {
+        assertEq(address(entryPoint).balance, 0);
+        assertEq(paymaster.getDeposit(), 0);
+
+        hoax(address(ownerAddress), 1 ether);
+        paymaster.deposit{value: 0.5 ether}();
+
+        assertEq(address(entryPoint).balance, 0.5 ether);
+        assertEq(paymaster.getDeposit(), 0.5 ether);
+    }
+
+    function testWithdraw() public {
+        testDeposit();
+
+        assertEq(address(entryPoint).balance, 0.5 ether);
+        assertEq(address(user).balance, 0);
+
+        vm.prank(address(ownerAddress));
+        paymaster.withdraw(payable(address(user)), 0.3 ether);
+
+        assertEq(address(user).balance, 0.3 ether);
+    }
+
+    function testWithdrawNotOwner() public {
+        testDeposit();
+
+        assertEq(address(entryPoint).balance, 0.5 ether);
+        assertEq(address(user).balance, 0);
+
+        vm.prank(address(notOwner));
+        vm.expectRevert();
+        paymaster.withdraw(payable(address(user)), 0.3 ether);
+
+        assertEq(address(entryPoint).balance, 0.5 ether);
+    }
+
+    function testWithdrawAll() public {
+        testDeposit();
+
+        assertEq(address(entryPoint).balance, 0.5 ether);
+        assertEq(address(user).balance, 0);
+
+        vm.prank(address(ownerAddress));
+        paymaster.withdrawAll(payable(address(user)));
+
+        assertEq(address(user).balance, 0.5 ether);
+        assertEq(address(entryPoint).balance, 0);
+    }
+
+    function testWithdrawAllNotOwner() public {
+        testDeposit();
+
+        assertEq(address(entryPoint).balance, 0.5 ether);
+        assertEq(address(user).balance, 0);
+
+        vm.prank(address(notOwner));
+        vm.expectRevert();
+        paymaster.withdrawAll(payable(address(user)));
+
+        assertEq(address(user).balance, 0);
+        assertEq(address(entryPoint).balance, 0.5 ether);
+    }
+
+    function testAddStake() public {
+        assertEq(paymaster.getStake(), 0);
+
+        hoax(address(ownerAddress), 1 ether);
+        paymaster.addStake{value: 0.5 ether}(120);
+
+        assertEq(paymaster.getStake(), 0.5 ether);
+        assertEq(entryPoint.getDepositInfo(address(paymaster)).staked, true);
+    }
+
+    function testAddStakeNotOwner() public {
+        assertEq(paymaster.getStake(), 0);
+
+        hoax(address(notOwner), 1 ether);
+        vm.expectRevert();
+        paymaster.addStake{value: 0.5 ether}(120);
+
+        assertEq(paymaster.getStake(), 0);
+    }
+
+    function testUnlockStake() public {
+        testAddStake();
+        assertEq(entryPoint.getDepositInfo(address(paymaster)).staked, true);
+        assertEq(paymaster.getStake(), 0.5 ether);
+
+        vm.prank(address(ownerAddress));
+        paymaster.unlockStake();
+        assertEq(entryPoint.getDepositInfo(address(paymaster)).staked, false);
+    }
+
+    function testUnlockStakeNotOwner() public {
+        testAddStake();
+        assertEq(paymaster.getStake(), 0.5 ether);
+
+        hoax(address(notOwner), 1 ether);
+        vm.expectRevert();
+        paymaster.unlockStake();
+    }
+
+    function testWithdrawStake() public {
+        assertEq(address(user).balance, 0);
+        testUnlockStake();
+
+        uint256 unstakeDelay = block.timestamp + 120;
+        vm.warp(unstakeDelay + 200);
+        vm.prank(address(ownerAddress));
+        paymaster.withdrawStake(payable(address(user)));
+
+        assertEq(address(user).balance, 0.5 ether);
+    }
+
+    function testWithdrawStakeNotOwner() public {
+        assertEq(address(user).balance, 0);
+
+        testUnlockStake();
+
+        uint256 unstakeDelay = block.timestamp + 120;
+        vm.warp(unstakeDelay + 200);
+        hoax(address(notOwner), 1 ether);
+        vm.expectRevert();
+        paymaster.withdrawStake(payable(address(user)));
+
+        assertEq(address(user).balance, 0);
+    }
+
+    ///
+    function generateUserOp() public returns(UserOperation memory userOp) {
+        UserOperation memory userOp;
+
+        userOp = UserOperation({
+            sender: address(wallet),
+            nonce: wallet.nonce(),
+            initCode: "",
+            callData: "",
+            callGasLimit: 2_000_000,
+            verificationGasLimit: 3_000_000,
+            preVerificationGas: 1_000_000,
+            maxFeePerGas: 1_000_105_660,
+            maxPriorityFeePerGas: 1_000_000_000,
+            paymasterAndData: "",
+            signature: ""
+        });
+    }
+
+    uint48 MOCK_VALID_UNTIL = 0x00000000deadbeef;
+    uint48 MOCK_VALID_AFTER = 0x0000000000001234;
+
+    function testParsePaymasterAndData() public {
+        UserOperation memory userOp;
+
+        userOp = generateUserOp();
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        bytes memory signature = createSignature(userOp, userOpHash, ownerPrivateKey, vm);
+        userOp.signature = signature;
+
+        bytes memory paymasterAndData = abi.encodePacked(
+            address(paymaster),
+            abi.encode(
+                MOCK_VALID_UNTIL,
+                MOCK_VALID_AFTER
+            ),
+            signature
+        );
+
+        (uint48 validUntil, uint48 validAfter, bytes memory sign) = 
+            paymaster.parsePaymasterAndData(paymasterAndData);
+
+        assertEq(validUntil, MOCK_VALID_UNTIL);
+        assertEq(validAfter, MOCK_VALID_AFTER);
+        assertEq(signature, sign);
+    }
+
+    function testValidatePaymasterUserOp() public {
+        UserOperation memory userOp;
+
+        userOp = generateUserOp();
+
+        bytes32 opHash = paymaster.getHash(userOp, MOCK_VALID_UNTIL, MOCK_VALID_AFTER);
+        bytes memory sign = createSignature2(opHash, ownerPrivateKey, vm);
+
+        userOp.paymasterAndData = abi.encodePacked(
+            address(paymaster),
+            abi.encode(
+                MOCK_VALID_UNTIL,
+                MOCK_VALID_AFTER
+            ),
+            sign
+        );
+
+        // Set remainder of test case
+        // address aggregator = address(0);
+        uint256 missingWalletFunds = 1096029019333521;
+
+        // Validate that the smart wallet can validate a userOperation
+        vm.prank(address(entryPoint));
+        (, uint256 validationData) = paymaster.validatePaymasterUserOp(userOp, opHash, missingWalletFunds);
+        
+        // entryPoint.simulateValidation(userOp);
+
+        ValidationData memory data;
+        data = _parseValidationData(validationData);
+        
+        console.log(data.aggregator);
+        console.log(data.validAfter);
+        console.log(data.validUntil);
+
+        assertEq(data.validUntil, MOCK_VALID_UNTIL);
+        assertEq(data.validAfter, MOCK_VALID_AFTER);
+    }
+}
