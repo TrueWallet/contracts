83d8bed376982f48469cf14a924d0865347ec533
VitalR <vitalii.rodikov@gmail.com>
2023-04-25 15:03:35 +0300

    test: verify proxy state after upgrade

--- test/unit/TrueWalletProxy.t.sol
+++ test/unit/TrueWalletProxy.t.sol
@@ -3,25 +3,26 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
 
-import {TrueWalletProxy} from "src/wallet/TrueWalletProxy.sol";
 import {TrueWallet} from "src/wallet/TrueWallet.sol";
+import {TrueWalletProxy} from "src/wallet/TrueWalletProxy.sol";
 import {TrueWalletFactory} from "src/wallet/TrueWalletFactory.sol";
 import {EntryPoint} from "src/entrypoint/EntryPoint.sol";
 import {MockWalletV2} from "../mock/MockWalletV2.sol";
+import {MockERC20} from "../mock/MockERC20.sol";
+import {MockERC721} from "../mock/MockERC721.sol";
 
 contract TrueWalletProxyUnitTest is Test {
-    TrueWalletFactory factory;
-    TrueWalletProxy proxy;
     TrueWallet wallet;
+    TrueWalletProxy proxy;
+    TrueWalletFactory factory;
     EntryPoint entryPoint;
     MockWalletV2 walletV2;
+    MockERC20 erc20token;
+    MockERC721 erc721token;
 
     address ownerAddress = 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955; // anvil account (7)
     uint256 ownerPrivateKey =
-        uint256(
-            0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
-        );
-    uint256 chainId = block.chainid;
+        uint256(0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356);
 
     bytes32 salt;
 
@@ -30,6 +31,8 @@ contract TrueWalletProxyUnitTest is Test {
         wallet = new TrueWallet();
         factory = new TrueWalletFactory(address(wallet), address(this));
         walletV2 = new MockWalletV2();
+        erc20token = new MockERC20();
+        erc721token = new MockERC721("Token", "TKN");
 
         bytes memory data = abi.encodeCall(
             TrueWallet.initialize,
@@ -44,7 +47,7 @@ contract TrueWalletProxyUnitTest is Test {
     }
 
     function deployWallet() public returns (TrueWallet proxyWallet) {
-        TrueWallet proxyWallet = factory.createWallet(
+        proxyWallet = factory.createWallet(
             address(entryPoint),
             ownerAddress,
             salt
@@ -136,5 +139,45 @@ contract TrueWalletProxyUnitTest is Test {
         vm.prank(address(notOwner));
         proxyWallet.withdrawETH(payable(address(entryPoint)), 1 ether);
         assertEq(address(entryPoint).balance, 1 ether);
+    }
+
+    function testProxyState() public {
+        TrueWallet proxyWallet = deployWallet();
+
+        assertEq(address(proxyWallet.entryPoint()), address(entryPoint));
+        assertEq(proxyWallet.owner(), ownerAddress);
+
+        assertEq(address(entryPoint).balance, 0);
+
+        vm.deal(address(proxyWallet), 2 ether);
+        assertEq(address(proxyWallet).balance, 2 ether);
+
+        erc20token.mint(address(proxyWallet), 1 ether);
+        assertEq(erc20token.balanceOf(address(proxyWallet)), 1 ether);
+
+        erc721token.safeMint(address(proxyWallet), 1237);
+
+        assertEq(address(proxyWallet).balance, 2 ether);
+        assertEq(erc20token.balanceOf(address(proxyWallet)), 1 ether);
+        assertEq(erc721token.ownerOf(1237), address(proxyWallet));
+        assertEq(erc721token.balanceOf(address(proxyWallet)), 1);
+
+        vm.startPrank(address(ownerAddress));
+        proxyWallet.upgradeTo(address(walletV2));
+
+        assertEq(address(proxyWallet).balance, 2 ether);
+        assertEq(erc20token.balanceOf(address(proxyWallet)), 1 ether);
+        assertEq(erc721token.ownerOf(1237), address(proxyWallet));
+        assertEq(erc721token.balanceOf(address(proxyWallet)), 1);
+        
+        address to = address(0xABCD);
+
+        proxyWallet.withdrawETH(payable(address(entryPoint)), 1 ether);
+        proxyWallet.withdrawERC20(address(erc20token), address(entryPoint), 1 ether);
+        proxyWallet.withdrawERC721(address(erc721token), 1237, to);
+
+        assertEq(address(proxyWallet).balance, 1 ether);
+        assertEq(erc20token.balanceOf(address(proxyWallet)), 0);
+        assertEq(erc721token.balanceOf(address(proxyWallet)), 0);
     }
 }
