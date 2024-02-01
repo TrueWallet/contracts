f3e20837055a1745f0e1d4bfd1426e2d912257ad
VitalR <vitalii.rodikov@gmail.com>
2024-02-01 19:42:57 +0200

    feat: update contract bytecode generator

--- src/deployer/DeployCodeGenerator.sol
+++ src/deployer/DeployCodeGenerator.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: LGPL-3.0
 pragma solidity ^0.8.19;
 
+import {TrueWallet} from "src/wallet/TrueWallet.sol";
 import {TrueWalletFactory} from "src/wallet/TrueWalletFactory.sol";
 import {TrueContractManager} from "src/registry/TrueContractManager.sol";
 import {SecurityControlModule} from "src/modules/SecurityControlModule/SecurityControlModule.sol";
 import {SocialRecoveryModule} from "src/modules/SocialRecoveryModule/SocialRecoveryModule.sol";
 
-// / @title A contract for deploying other contracts using CREATE3.
-// / @notice This contract allows an owner to deploy contracts to deterministic addresses using CREATE3.
+/// @title Deploy Code Generator.
+/// @notice Provides functions to generate creation bytecode for various contracts.
 contract DeployCodeGenerator {
     /// @notice Retrieves the creation bytecode of the TrueWalletFactory contract.
+    /// @dev Combines TrueWalletFactory creation bytecode with encoded constructor parameters.
+    /// @param _walletImpl Address of the wallet implementation contract.
+    /// @param _owner Address of the owner for the TrueWalletFactory contract.
+    /// @param _entryPoint Address of the entry point contract.
     /// @return bytecode The bytecode used to deploy the TrueWalletFactory contract.
     function getTrueWalletFactoryCode(address _walletImpl, address _owner, address _entryPoint)
         external
@@ -21,6 +26,8 @@ contract DeployCodeGenerator {
     }
 
     /// @notice Retrieves the creation bytecode of the TrueContractManager contract.
+    /// @dev Combines TrueContractManager creation bytecode with encoded constructor parameter.
+    /// @param _owner Address of the owner for the TrueContractManager contract.
     /// @return bytecode The bytecode used to deploy the TrueContractManager contract.
     function getTrueContractManagerCode(address _owner) external pure returns (bytes memory) {
         bytes memory encodeInitParams = abi.encode(address(_owner));
@@ -28,6 +35,8 @@ contract DeployCodeGenerator {
     }
 
     /// @notice Retrieves the creation bytecode of the SecurityControlModule contract.
+    /// @dev Combines SecurityControlModule creation bytecode with encoded constructor parameter.
+    /// @param _trueContractManager Address of the TrueContractManager contract.
     /// @return bytecode The bytecode used to deploy the SecurityControlModule contract.
     function getSecurityControlModuleCode(address _trueContractManager) external pure returns (bytes memory) {
         bytes memory encodeInitParams = abi.encode(address(_trueContractManager));
@@ -35,8 +44,16 @@ contract DeployCodeGenerator {
     }
 
     /// @notice Retrieves the creation bytecode of the SocialRecoveryModule contract.
+    /// @dev Returns the creation bytecode of the SocialRecoveryModule contract.
     /// @return bytecode The bytecode used to deploy the SocialRecoveryModule contract.
     function getSocialRecoveryModuleCode() external pure returns (bytes memory) {
         return type(SocialRecoveryModule).creationCode;
+    }
+
+    /// @notice Retrieves the creation bytecode of the TrueWallet contract.
+    /// @dev Returns the creation bytecode of the TrueWallet contract.
+    /// @return bytecode The bytecode used to deploy the TrueWallet contract.
+    function getTrueWalletImplCode() external pure returns (bytes memory) {
+        return type(TrueWallet).creationCode;
     }
 }
